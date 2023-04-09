82c403996a1f7e599e7dc7f7115b842dedb095b7
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-09 12:06:03 +0530

    updating ./roboshop-mutable-infra

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
-  private_subnets = lookup(lookup({ for k,v  in module.vpc.private_subnets : k.subnets => v.subnets } , "subnets", null), "apps" , null)
+  private_subnets = lookup(lookup(lookup(lookup({ for k,v  in module.vpc.private_subnets : k.subnets => v.subnets } ,
+    "subnets", null), "apps" , null),"subnets",null),"id" ,null)
 }
