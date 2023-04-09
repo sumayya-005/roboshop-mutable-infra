e5c4293557800ceb579f4109abce7a57303e1bbe
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-09 12:11:55 +0530

    updating ./roboshop-mutable-infra

--- locals.tf
+++ locals.tf
@@ -1,4 +1,13 @@
 locals {
-  private_subnets = lookup(lookup(lookup(lookup({ for k,v  in module.vpc.private_subnets : k.subnets => v.subnets } ,
+  app_private_subnets = lookup(lookup(lookup(lookup({ for k,v  in module.vpc.private_subnets : k.subnets => v.subnets } ,
     "subnets", null), "apps" , null),"subnets",null),"id" ,null)
+  database_private_subnets = lookup(lookup(lookup(lookup({ for k,v  in module.vpc.private_subnets : k.subnets => v.subnets } ,
+    "subnets", null), "database" , null),"subnets",null),"id" ,null)
+  frontend_private_subnets = lookup(lookup(lookup(lookup({ for k,v  in module.vpc.private_subnets : k.subnets => v.subnets } ,
+    "subnets", null), "frontend" , null),"subnets",null),"id" ,null)
+  public_subnets = lookup({ for k,v  in module.vpc.public_subnets : k.subnets => v.subnets } , "subnets", null)
+}
+
+output "public_subnets" {
+  value = local.public_subnets
 }
