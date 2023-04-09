e86da3e8fcaabc241ef56dab7d841ed1dfac4030
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-09 12:13:59 +0530

    updating ./roboshop-mutable-infra

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ module "vpc"  {
   management_vpc = var.management_vpc
 }
 
-#module "docdb"  {
-#  source   = "./vendor/modules/docdb"
-#  docdb    = var.docdb
-#  env      = var.env
-# subnets   = lookup (local.subnets, "database" ,null)
-#}
+module "docdb"  {
+  source   = "./vendor/modules/docdb"
+  docdb    = var.docdb
+  env      = var.env
+ subnets   = local.database_private_subnets[*].id
+}
 
 output "app_private_subnets" {
   value = local.private_subnets[*].id
