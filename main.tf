c2927f4587c85ea2d728c69659f4691ba54415e3
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-10 11:57:19 +0530

    updating ./roboshop-mutable-infra

--- main.tf
+++ main.tf
@@ -1,20 +1,25 @@
-module "vpc"  {
+module "vpc" {
+  for_each = var.vpc
   source = "./vendor/modules/vpc"
-  vpc    = var.vpc
+  cidr_block = each.value.cidr_block
+  additional_cidr_block = each.value. additonal_cidr_block
+  subnet_availability_zones = each.value.subnet_availability_zones
+  private_subnets = each.value.private_subnets
+  public_subnets = each.value.public_subnets
   env    = var.env
   management_vpc = var.management_vpc
 }
 
-module "docdb"  {
-  source   = "./vendor/modules/docdb"
-  docdb    = var.docdb
-  env      = var.env
- subnets   = local.database_private_subnets["*"].id
-}
-
-module "rds" {
-  source = "./vendor/modules/rds"
-  rds    = var.rds
-  env    = var.env
-  subnets = local.database_private_subnets["*"].id
-}
+#module "docdb"  {
+#  source   = "./vendor/modules/docdb"
+#  docdb    = var.docdb
+#  env      = var.env
+# subnets   = local.database_private_subnets[*].id
+#}
+#
+#module "rds" {
+#  source = "./vendor/modules/rds"
+#  rds    = var.rds
+#  env    = var.env
+#  subnets = local.database_private_subnets[*].id
+#}
