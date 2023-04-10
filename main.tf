94b6c01be7b42862d6960960af10a17593a58445
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-10 12:37:37 +0530

    updating ./roboshop-mutable-infra

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "vpc" {
-  for_each = var.vpc
-  source = "./vendor/modules/vpc"
-  cidr_block = each.value.cidr_block
-  additional_cidr_block = each.value.additional_cidr_block
+  for_each                  = var.vpc
+  source                    = "./vendor/modules/vpc"
+  cidr_block                = each.value.cidr_block
+  additional_cidr_block     = each.value.additional_cidr_block
+  private_subnets           = each.value.private_subnets
+  public_subnets            = each.value.public_subnets
   subnet_availability_zones = each.value.subnet_availability_zones
-  private_subnets = each.value.private_subnets
-  public_subnets  = each.value.public_subnets
-  env    = var.env
-  management_vpc = var.management_vpc
+  env                       = var.env
+  management_vpc            = var.management_vpc
 }
 
 #module "docdb"  {
