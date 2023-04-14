5dbe1d351cff8210edfb0f097d02de2a36466d84
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-14 16:22:36 +0530

    updating ./roboshop-mutable-infra

--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "vpc" {}
 variable "env" {}
 variable "management_vpc" {}
-#variable "docdb" {}
+variable "docdb" {}
 #variable "rds" {}
 #variable "elasticache" {}
 #variable "rabbitmq" {}
