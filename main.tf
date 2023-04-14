c8bace958500703e840b18ed92ed7c92ca64e12f
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-14 16:06:25 +0530

    updating ./roboshop-mutable-infra

--- main.tf
+++ main.tf
@@ -10,76 +10,76 @@ module "vpc" {
   management_vpc            = var.management_vpc
 }
 
-module "docdb" {
-  for_each = var.docdb
-  source   = "./vendor/modules/docdb"
-  name     = each.key
-  engine   = each.value.engine
-  env      = var.env
-  subnets  = flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
-}
-
-
-module "rds" {
-  source              = "./vendor/modules/rds"
-  for_each            = var.rds
-  env                 = var.env
-  subnets             = flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
-  name                = each.key
-  allocated_storage   = each.value.allocated_storage
-  engine              = each.value.engine
-  engine_version      = each.value.engine_version
-  instance_class      = each.value.instance_class
-  skip_final_snapshot = each.value.skip_final_snapshot
-  port_no             = each.value.app_port_no
-}
-
-module "elasticache" {
-  source = "./vendor/modules/elasticache"
-  for_each = var.elasticache
-  env =var.env
-  subnets= flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
-  name = each.key
-  engine          = each.value.engine
-  engine_version  = each.value. engine_version
-  node_type       = each.value.node_type
-  num_cache_nodes = each.value.num_cache_nodes
-}
-
-module "rabbitmq" {
-  source = "./vendor/modules/rabbitmq"
-  for_each = var.rabbitmq
-  env      = var.env
-  subnets  = flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
-  name = each.key
-  instance_type =each.value.instance_type
-}
-
-
-module "apps" {
-  source  = "./vendor/modules/app-setup"
-  env     = var.env
-  subnets = each.key =="frontend" ? flatten([for i, j in module.vpc : j.private_subnets["frontend"]["subnets"][*]
-  .id]) : flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
-  for_each        = var.apps
-  name            = each.key
-  instance_type   = each.value.instance_type
-  min_size        = each.value.min_size
-  max_size        = each.value.max.size
-  vpc_id          = element([for i, j in module.vpc : j.vpc_id], 0)
-  BASTION_NODE    = var.BASTION_NODE
-  app_port_no     = each.value.app_port_no
-  PROMETHEUS_NODE = var.PROMETHEUES_NODE
-  vpc_cidr        = element([for i, j in module.vpc : j.vpc_id], 0)
-}
-
-module "alb" {
-  source = "./vendor/modules/alb"
-  for_each = var.alb
-  env     = var.env
-  subnets        = each.value.subnets
-  name           = each.key
-  vpc_id         = element([for i, j in module.vpc : j.vpc_id], 0)
-  vpc_cidr       = element([for i, j in module.vpc : j.vpc_cidr], 0)
-  internal       = each.value.internal
-}
+#module "docdb" {
+#  for_each = var.docdb
+#  source   = "./vendor/modules/docdb"
+#  name     = each.key
+#  engine   = each.value.engine
+#  env      = var.env
+#  subnets  = flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
+#}
+#
+#
+#module "rds" {
+#  source              = "./vendor/modules/rds"
+#  for_each            = var.rds
+#  env                 = var.env
+#  subnets             = flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
+#  name                = each.key
+#  allocated_storage   = each.value.allocated_storage
+#  engine              = each.value.engine
+#  engine_version      = each.value.engine_version
+#  instance_class      = each.value.instance_class
+#  skip_final_snapshot = each.value.skip_final_snapshot
+#  port_no             = each.value.app_port_no
+#}
+#
+#module "elasticache" {
+#  source = "./vendor/modules/elasticache"
+#  for_each = var.elasticache
+#  env =var.env
+#  subnets= flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
+#  name = each.key
+#  engine          = each.value.engine
+#  engine_version  = each.value. engine_version
+#  node_type       = each.value.node_type
+#  num_cache_nodes = each.value.num_cache_nodes
+#}
+#
+#module "rabbitmq" {
+#  source = "./vendor/modules/rabbitmq"
+#  for_each = var.rabbitmq
+#  env      = var.env
+#  subnets  = flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
+#  name = each.key
+#  instance_type =each.value.instance_type
+#}
+#
+#
+#module "apps" {
+#  source  = "./vendor/modules/app-setup"
+#  env     = var.env
+#  subnets = each.key =="frontend" ? flatten([for i, j in module.vpc : j.private_subnets["frontend"]["subnets"][*]
+#  .id]) : flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
+#  for_each        = var.apps
+#  name            = each.key
+#  instance_type   = each.value.instance_type
+#  min_size        = each.value.min_size
+#  max_size        = each.value.max.size
+#  vpc_id          = element([for i, j in module.vpc : j.vpc_id], 0)
+#  BASTION_NODE    = var.BASTION_NODE
+#  app_port_no     = each.value.app_port_no
+#  PROMETHEUS_NODE = var.PROMETHEUES_NODE
+#  vpc_cidr        = element([for i, j in module.vpc : j.vpc_id], 0)
+#}
+#
+#module "alb" {
+#  source = "./vendor/modules/alb"
+#  for_each = var.alb
+#  env     = var.env
+#  subnets        = each.value.subnets
+#  name           = each.key
+#  vpc_id         = element([for i, j in module.vpc : j.vpc_id], 0)
+#  vpc_cidr       = element([for i, j in module.vpc : j.vpc_cidr], 0)
+#  internal       = each.value.internal
+#}
