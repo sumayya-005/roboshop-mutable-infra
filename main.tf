785c45cad3455489f176fa4f9b44145209cd99d3
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-18 16:14:41 +0530

    updating ./roboshop-mutable-infra

--- main.tf
+++ main.tf
@@ -75,6 +75,7 @@ module "apps" {
   vpc_cidr        = element([for i, j in module.vpc : j.vpc_id], 0)
   private_zone_id  = var.private_zone_id
   public_dns_name   = each.value.public_dns_name
+  public_zone_id   = var.public_zone_id
 }
 
 module "alb" {
