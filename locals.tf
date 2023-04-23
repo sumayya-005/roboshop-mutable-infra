394b800268b8b1c3e7e16335a8a97b17899c1fa2
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-23 16:33:57 +0530

    updating ./roboshop-mutable-infra

--- locals.tf
+++ locals.tf
@@ -16,11 +16,11 @@ locals {
   alb = {
     public = {
       vpc_cidr = "0.0.0.0/0"
-      subnets  = flatten([for i, j in module.vpc : j.public_subnets["public"]["subnets"][*].id])
+#      subnets  = flatten([for i, j in module.vpc : j.public_subnets["public"]["subnets"][*].id])
     }
     private = {
       vpc_cidr = element([for i, j in module.vpc : j.vpc_cidr], 0)
-      subnets  = flatten([for i, j in module.vpc : j.private_subnets["app"]["subnets"][*].id])
+#      subnets  = flatten([for i, j in module.vpc : j.private_subnets["app"]["subnets"][*].id])
     }
   }
   merged_alb = tomap({
