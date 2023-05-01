117bb72cca6964ab24c1275ae02a5689872a5628
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-01 11:43:47 +0530

    updating ./roboshop-mutable-infra

--- vars.tf
+++ vars.tf
@@ -6,8 +6,8 @@ variable "rds" {}
 variable "elasticache" {}
 variable "rabbitmq" {}
 variable "apps" {}
-#variable "BASTION_NODE" {}
-#variable "PROMETHEUES_NODE" {}
+variable "BASTION_NODE" {}
+variable "PROMETHEUES_NODE" {}
 #variable "alb" {}
 variable "private_zone_id" {}
 #variable "public_zone_id" {}
