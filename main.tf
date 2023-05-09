0b0300fdc966db4e84137ef23393b01877530ee7
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-09 17:42:27 +0530

    updating ./roboshop-mutable-infra

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ module "alb" {
   for_each       = var.alb
   env            = var.env
   name           = each.key
-#  subnets        = each.value.subnets
+  subnets        = each.value.subnets
   vpc_id         = element([for i, j in module.vpc : j.vpc_id], 0)
   vpc_cidr       = element([for i, j in module.vpc : j.vpc_cidr], 0)
   internal       = each.value.internal
