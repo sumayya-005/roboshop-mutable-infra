a26b5a82da6ae846d259e3a531179f6c8a927e27
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-12 11:34:25 +0530

    updating ./roboshop-mutable-infra

--- main.tf
+++ main.tf
@@ -102,16 +102,18 @@ module"apps" {
 #  })
 #}
 
-#module "alb" {
-#  source         = "./vendor/modules/alb"
-#  for_each       = var.alb
-#  env            = var.env
-#  name           = each.key
+module "alb" {
+  source         = "./vendor/modules/alb"
+  for_each       = var.alb
+  env            = var.env
+  name            = each.key
+  private_subnets = flatten([for i,j in module.vpc : j.private_subnets["app"]["subnets"][*].id])
+  public_subnets  = flatten([for i,j in module.vpc : j.public_subnets["public"]["subnets"][*].id])
 #  subnets        = each.value.subnets
-#  vpc_id         = element([for i, j in module.vpc : j.vpc_id], 0)
-#  vpc_cidr       = element([for i, j in module.vpc : j.vpc_cidr], 0)
-#  internal       = each.value.internal
-#}
+  vpc_id         = element([for i, j in module.vpc : j.vpc_id], 0)
+  vpc_cidr       = element([for i, j in module.vpc : j.vpc_cidr], 0)
+  internal       = each.value.internal
+}
 
 #output "test" {
 #  value = local.merged_alb
