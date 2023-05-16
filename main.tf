487012ad7c4e83f0531107b4475cc104e3668e7f
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-16 16:40:10 +0530

    updating ./roboshop-mutable-infra

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ module "docdb" {
   engine   = each.value.engine
   env      = var.env
   subnets  = flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
-# nodes     = each.value.nodes
-#  skip_final_snapshot = each.value.skip_final_snapshot
+ nodes     = each.value.nodes
+  skip_final_snapshot = each.value.skip_final_snapshot
 #  vpc_id        = element([for i, j in module.vpc : j.vpc_id], 0)
 #  vpc_cidr        = element([for i, j in module.vpc : j.vpc_id], 0)
 #  BASTION_NODE  = var.BASTION_NODE
